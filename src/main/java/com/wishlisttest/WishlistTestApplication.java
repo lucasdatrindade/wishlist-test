80d7237879d870b0cc0c1837118c690245071df3
lucas <macuser@10.1.102.22>
2023-05-19 10:11:23 -0300

    Initial commit

---
+++ src/main/java/com/wishlisttest/WishlistTestApplication.java
@@ -0,0 +1,13 @@
+package com.wishlisttest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WishlistTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WishlistTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wishlisttest/WishlistTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wishlisttest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WishlistTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
